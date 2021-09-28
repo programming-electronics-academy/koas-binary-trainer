2b70a61549f2b0e8ed4a4da3bb06b6259cfa17e0
Michael Cheich <michaelcheich@Michaels-iMac-2.lan1>
2021-09-28 09:40:15 -0400

    first commit

---
+++ binaryTrainer.ino
@@ -0,0 +1,19 @@
+/**
+ * 
+ * An Arduino Board Game using the koas to practice your binary skills!
+ * 
+ * TODO:
+ *  
+ *  
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
