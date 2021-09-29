2fef4660d5ab58fa1bc2d38e1e3223d97fb10937
Michael Cheich <michaelcheich@Michaels-iMac-2.local>
2021-09-29 10:00:55 -0400

    seed and create random number

---
+++ binaryTrainer/ToDo.ino
@@ -0,0 +1,36 @@
+/*
+ * ****************
+ * * TODO
+ * ****************
+ * 
+ * 
+ * Generate Random # between 0 and 255
+ *  MODES...
+ *    TEST
+ *    PRACTICE
+ *      Generate and Display
+ *      Wait to Enter
+ *      Enter and Display
+ *      Result
+ *        Display Correct
+ *          Serial Message
+ *          LED dance
+ *          Piezo Happy Song
+ *        Display InCorrect
+ *          Serial Message
+ *          LED Flash
+ *          Piezo Sad Song
+ *      
+ * Wait for input
+ *  Press Right Button to input 1
+ *  Press Left Button to input 0
+ *  If Right Button Press
+ * 
+ * 
+ * ***********
+ * * DONE
+ * **********
+ * 
+ * * Set Button Pins and Mode
+ * * Set LED Pins and Modes
+ */
--- binaryTrainer/binaryTrainer.ino
+++ binaryTrainer/binaryTrainer.ino
@@ -1,7 +1,9 @@
 const byte RIGHT_BTN = 5; // Right Button -> press for 1 | YES
 const byte LEFT_BTN = 2; // Left Button -> press for 0 | NO
 
-const byte LEDPins[8] = { 6, 7, 8, 9, 10, 11, 12, 13};
+const byte LEDPins[8] = { 6, 7, 8, 9, 10, 11, 12, 13 };
+
+const byte analogPinForRandomSeed = A2;
 
 void setup() {
 
@@ -16,8 +18,14 @@ void setup() {
     pinMode(LEDPins[i], OUTPUT);
   }
 
+  //Seed random number
+  randomSeed(analogRead(analogPinForRandomSeed));
 }
 
 void loop() {
 
+  //Create and save random Number
+  byte practiceByte = random(0, 256);
+  Serial.println(practiceByte);
+  
 }
