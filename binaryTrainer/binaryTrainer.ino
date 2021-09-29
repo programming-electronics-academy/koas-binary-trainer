9adc6f7f132e5808fbd514f8dee4c5867589179b
Michael Cheich <michaelcheich@Michaels-iMac-2.local>
2021-09-29 09:50:26 -0400

    set LED and BTN pins and modes

--- binaryTrainer/binaryTrainer.ino
+++ binaryTrainer/binaryTrainer.ino
@@ -1,9 +1,20 @@
 const byte RIGHT_BTN = 5; // Right Button -> press for 1 | YES
 const byte LEFT_BTN = 2; // Left Button -> press for 0 | NO
 
+const byte LEDPins[8] = { 6, 7, 8, 9, 10, 11, 12, 13};
+
 void setup() {
 
   Serial.begin(9600);
+
+  //Set pinModes
+  pinMode(RIGHT_BTN, INPUT);
+  pinMode(LEFT_BTN, INPUT);
+
+  for (int i = 0; i < sizeof(LEDPins); i++)
+  {
+    pinMode(LEDPins[i], OUTPUT);
+  }
 
 }
 
